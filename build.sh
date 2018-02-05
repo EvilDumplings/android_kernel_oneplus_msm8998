43b7e5e6739e8f44123c6dce6c887a2599ca19cf
Michele Beccalossi <beccalossi.michele@gmail.com>
2018-02-05 11:35:54 +0100

    build: split the script into functions
    Signed-off-by: Michele Beccalossi <beccalossi.michele@gmail.com>

--- build.sh
+++ build.sh
@@ -24,63 +24,103 @@ export CROSS_COMPILE=$BUILD_CROSS_COMPILE/bin/aarch64-linux-android-;
 
 # # # VERIFY TOOLCHAIN PRESENCE # # #
 
-if [ ! -d "$BUILD_CROSS_COMPILE" ]; then
-  git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 $BUILD_CROSS_COMPILE;
-else
-  cd $BUILD_CROSS_COMPILE;
-  git pull;
-  cd $BUILD_KERNEL_DIR;
-fi;
+FUNC_VERIFY_TOOLCHAIN()
+{
+  if [ ! -d "$BUILD_CROSS_COMPILE" ]; then
+    git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 $BUILD_CROSS_COMPILE;
+  else
+    cd $BUILD_CROSS_COMPILE;
+    git pull;
+    cd $BUILD_KERNEL_DIR;
+  fi;
+}
 
 
 # # # VERIFY ZIP TEMPLATE PRESENCE # # #
 
-if [ ! -d "$BUILD_ZIP_DIR" ]; then
-  git clone https://github.com/EvilDumplings/AnyKernel2.git $BUILD_ZIP_DIR;
-else
-  cd $BUILD_ZIP_DIR;
-  git pull;
-  cd $BUILD_KERNEL_DIR;
-fi;
+FUNC_VERIFY_TEMPLATE()
+{
+  if [ ! -d "$BUILD_ZIP_DIR" ]; then
+    git clone https://github.com/EvilDumplings/AnyKernel2.git $BUILD_ZIP_DIR;
+  else
+    cd $BUILD_ZIP_DIR;
+    git pull;
+    cd $BUILD_KERNEL_DIR;
+  fi;
+}
 
 
 # # # CLEAN BUILD OUTPUT # # #
 
-rm -rf $BUILD_KERNEL_OUT_DIR;
-rm -f $KERNEL_IMG;
-rm -rf $KERNEL_MODULES/*;
-rm -rf $PRODUCT_OUT/*.zip;
+FUNC_CLEAN_OUTPUT()
+{
+  rm -rf $BUILD_KERNEL_OUT_DIR;
+  rm -f $KERNEL_IMG;
+  rm -rf $KERNEL_MODULES/*;
+  rm -rf $PRODUCT_OUT/*.zip;
+}
 
 
 # # # BUILD CONFIG AND KERNEL # # #
 
-mkdir -p $BUILD_KERNEL_OUT_DIR;
+FUNC_BUILD()
+{
+  mkdir -p $BUILD_KERNEL_OUT_DIR;
 
-make O=$BUILD_KERNEL_OUT_DIR primal_oneplus5_defconfig;
-make O=$BUILD_KERNEL_OUT_DIR -j$BUILD_JOB_NUMBER;
+  make O=$BUILD_KERNEL_OUT_DIR primal_oneplus5_defconfig;
+  make O=$BUILD_KERNEL_OUT_DIR -j$BUILD_JOB_NUMBER;
+}
 
 
 # # # STRIP AND RESIGN MODULES # # #
 
-find $BUILD_KERNEL_OUT_DIR \
-    -name "*.ko" \
-    -exec ${CROSS_COMPILE}strip --strip-unneeded {} \;
-find $BUILD_KERNEL_OUT_DIR \
-    -name "*.ko"  \
-    -exec $BUILD_KERNEL_OUT_DIR/scripts/sign-file sha512 $BUILD_KERNEL_OUT_DIR/certs/signing_key.pem $BUILD_KERNEL_OUT_DIR/certs/signing_key.x509 {} \;
+FUNC_STRIP_MODULES()
+{
+  find $BUILD_KERNEL_OUT_DIR \
+      -name "*.ko" \
+      -exec ${CROSS_COMPILE}strip --strip-unneeded {} \;
+}
+FUNC_SIGN_MODULES()
+{
+  find $BUILD_KERNEL_OUT_DIR \
+      -name "*.ko"  \
+      -exec $BUILD_KERNEL_OUT_DIR/scripts/sign-file sha512 $BUILD_KERNEL_OUT_DIR/certs/signing_key.pem $BUILD_KERNEL_OUT_DIR/certs/signing_key.x509 {} \;
+}
 
 
 # # # COPY BUILD OUTPUT # # #
 
-cp $BUILD_KERNEL_OUT_DIR/arch/arm64/boot/Image.gz-dtb $KERNEL_IMG;
-find $BUILD_KERNEL_OUT_DIR \
-    -name "*.ko" \
-    -exec cp {} $KERNEL_MODULES \;
+FUNC_COPY_KERNEL()
+{
+  cp $BUILD_KERNEL_OUT_DIR/arch/arm64/boot/Image.gz-dtb $KERNEL_IMG;
+}
+FUNC_COPY_MODULES()
+{
+  find $BUILD_KERNEL_OUT_DIR \
+      -name "*.ko" \
+      -exec cp {} $KERNEL_MODULES \;
+}
 
 
 # # # BUILD ZIP # # #
 
-cd $BUILD_ZIP_DIR;
-zip -r9 $PRODUCT_OUT/primal-oneplus5-custom-v1.0.0.zip * \
-    -x patch/* ramdisk/* *.placeholder
-cd $BUILD_KERNEL_DIR;
+FUNC_BUILD_ZIP()
+{
+  cd $BUILD_ZIP_DIR;
+  zip -r9 $PRODUCT_OUT/primal-oneplus5-custom-v1.0.0.zip * \
+      -x patch/* ramdisk/* *.placeholder
+  cd $BUILD_KERNEL_DIR;
+}
+
+# MAIN FUNCTION
+(
+  FUNC_VERIFY_TOOLCHAIN;
+  FUNC_VERIFY_TEMPLATE;
+  FUNC_CLEAN_OUTPUT;
+  FUNC_BUILD;
+  FUNC_STRIP_MODULES;
+  FUNC_SIGN_MODULES;
+  FUNC_COPY_KERNEL;
+  FUNC_COPY_MODULES;
+  FUNC_BUILD_ZIP;
+)
